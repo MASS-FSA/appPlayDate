b616b5218ba37f683066c11a0a7528bedf8cde1d
Mehron Latifi <88285109+MehronS@users.noreply.github.com>
2021-11-24 16:22:17 -0500

    put lat and lng to hook/state

--- client/components/UserPage.js
+++ client/components/UserPage.js
@@ -2,11 +2,13 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 
-let myMap, latitude, longitude;
+let myMap;
 
 export const UserPage = (props) => {
   const [filter, setFilter] = useState("1");
   const [users, setUsers] = useState([]);
+  const [nearbyUsers, setNearbyUsers] = useState([]);
+  const [[lat, lng], setCoords] = useState([[]]);
 
   useEffect(() => {
     function getLocation() {
@@ -20,11 +22,11 @@ export const UserPage = (props) => {
     async function loadMap(position) {
       const { data } = await axios.get(`/api/users`);
       setUsers(data);
-      console.log(users);
 
-      latitude = position.coords.latitude;
-      longitude = position.coords.longitude;
-      console.log(latitude, longitude);
+      let latitude = position.coords.latitude;
+      let longitude = position.coords.longitude;
+
+      setCoords([latitude, longitude]);
 
       myMap = L.map("map").setView([latitude, longitude], 13);
 
@@ -40,32 +42,30 @@ export const UserPage = (props) => {
   }, []);
 
   function handleChange(event) {
+    console.log(`state coords`, lat, lng);
     setFilter(event.target.value);
 
-    users
-      .filter((person) => {
-        console.log(event.target.value);
-        return (
+    const nearby = users.filter((person) => {
+      console.log(event.target.value);
+      return (
+        (
+          myMap.distance([lat, lng], [person.latitude, person.longitude]) / 1609
+        ).toFixed(2) <= event.target.value
+      );
+    });
+
+    nearby.map((person) => {
+      console.log(
+        `${person.username} is ` +
           (
-            myMap.distance(
-              [latitude, longitude],
-              [person.latitude, person.longitude]
-            ) / 1609
-          ).toFixed(2) <= event.target.value
-        );
-      })
-      .map((person) => {
-        console.log(
-          `${person.username} is ` +
-            (
-              myMap.distance(
-                [latitude, longitude],
-                [person.latitude, person.longitude]
-              ) / 1609
-            ).toFixed(2) +
-            ` miles from Mehron`
-        );
-      });
+            myMap.distance([lat, lng], [person.latitude, person.longitude]) /
+            1609
+          ).toFixed(2) +
+          ` miles from Mehron`
+      );
+    });
+
+    setNearbyUsers(nearby);
   }
 
   // useEffect(() => {
@@ -84,6 +84,16 @@ export const UserPage = (props) => {
         </select>
       </div>
       <div id="map"></div>
+      <div>
+        {nearbyUsers.map((person) => {
+          return (
+            <div key={person.id} className="nearby_users">
+              <img src={person.image} />
+              <h6>{person.username}</h6>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
