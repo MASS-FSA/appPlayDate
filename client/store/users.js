b8d69a3d195aaf051c56dcab6ec36aa13a24656d
Mehron Latifi <88285109+MehronS@users.noreply.github.com>
2021-11-29 17:10:41 -0500

    added backend route to find users via lat/lng basic formula. created store and also made a circle appear on map

--- client/store/users.js
+++ client/store/users.js
@@ -1,5 +1,8 @@
+import axios from "axios";
+
 // ACTION TYPES
 const SET_ALL_USERS = `SET_ALL_USERS`;
+const SET_NEARBY_USERS = `SET_NEARBY_USERS`;
 
 // ACTION CREATORS
 export const setAllUsers = (users) => {
@@ -9,14 +12,42 @@ export const setAllUsers = (users) => {
   };
 };
 
+export const setNearbyUsers = (users) => {
+  return {
+    type: SET_NEARBY_USERS,
+    users,
+  };
+};
+
+// THUNKS
+
+export const fetchdUsersWithinDistance = (userId, distance) => {
+  const distanceObj = { distance };
+  return async (dispatch) => {
+    try {
+      const { data } = await axios.post(
+        `/api/users/nearby/${userId}`,
+        distanceObj
+      );
+
+      dispatch(setNearbyUsers(data));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+};
+
 const initialState = {
   allUsers: [],
+  nearbyUsers: [],
 };
 
 export default (state = initialState, action) => {
   switch (action.type) {
     case SET_ALL_USERS:
       return { ...state, allUsers: action.users };
+    case SET_NEARBY_USERS:
+      return { ...state, nearbyUsers: action.users };
 
     default:
       return state;
