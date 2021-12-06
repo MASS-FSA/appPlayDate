5325cab9391c23ded09930a741fd8fd4db59115c
Sean Hagstrom <seanhagstrom1@gmail.com>
2021-12-06 17:50:43 -0500

    updated findNearByUsers

--- client/Routes.js
+++ client/Routes.js
@@ -1,17 +1,17 @@
-import React, { Component, Fragment } from "react";
-import { connect } from "react-redux";
-import { withRouter, Route, Switch, Redirect } from "react-router-dom";
-import AllEvents from "./components/AllEvents";
-import { Login, Signup } from "./components/AuthForm";
-import Chat from "./components/Chat";
-import CreateEvent from "./components/CreateEvent";
-import Questionaire from "./components/Questionaire";
-import SingleEvent from "./components/SingleEvent";
-import UserPage from "./components/UserPage";
-import Places from "./components/places";
-import UserProfile from "./components/UserProfile";
-import { me } from "./store";
-import OthersProfile from "./components/OthersProfile";
+import React, { Component, Fragment } from 'react';
+import { connect } from 'react-redux';
+import { withRouter, Route, Switch, Redirect } from 'react-router-dom';
+import AllEvents from './components/AllEvents';
+import { Login, Signup } from './components/AuthForm';
+import Chat from './components/Chat';
+import CreateEvent from './components/CreateEvent';
+import Questionaire from './components/Questionaire';
+import SingleEvent from './components/SingleEvent';
+import UserPage from './components/UserPage';
+import Places from './components/places';
+import UserProfile from './components/UserProfile';
+import { me } from './store';
+import OthersProfile from './components/OthersProfile';
 
 /**
  * COMPONENT
@@ -28,24 +28,21 @@ class Routes extends Component {
       <div>
         {isLoggedIn ? (
           <Switch>
-            <Route exact path="/home" component={UserPage} />
-            <Route exact path="/events" component={AllEvents} />
-            <Route exact path="/events/create" component={CreateEvent} />
-            <Route exact path="/events/:id" component={SingleEvent} />
-            <Route path="/intake" component={Questionaire} />
-            <Route exact path="/places" component={Places} />
-            <Route path="/chat" component={Chat} />
-            <Route exact path="/profile/:userId" component={OthersProfile} />
-            <Route exact path="/myProfile" component={UserProfile} />
+            <Route exact path='/home' component={UserPage} />
+            <Route exact path='/events' component={AllEvents} />
+            <Route exact path='/events/create' component={CreateEvent} />
+            <Route exact path='/events/:id' component={SingleEvent} />
+            <Route path='/intake' component={Questionaire} />
+            <Route exact path='/places' component={Places} />
+            <Route path='/chat' component={Chat} />
+            <Route exact path='/profile/:userId' component={OthersProfile} />
+            <Route exact path='/myProfile' component={UserProfile} />
           </Switch>
         ) : (
           <Switch>
-            <Route path="/" exact component={Login} />
-            <Route path="/login" component={Login} />
-            <Route path="/signup" component={Signup} />
-            <Route path="/chat" component={Chat} />
-
-            <Route exact path="/places" component={Places} />
+            <Route path='/' exact component={Login} />
+            <Route path='/login' component={Login} />
+            <Route path='/signup' component={Signup} />
           </Switch>
         )}
       </div>
--- client/components/UserPage.js
+++ client/components/UserPage.js
@@ -1,7 +1,7 @@
-import React, { createRef, useEffect, useState } from "react";
-import { connect } from "react-redux";
-import { fetchdUsersWithinDistance, updateSingleUser } from "../store/users";
-import { loadMap, getGeoLocationFromBrowser } from "../../Util/loadMap";
+import React, { createRef, useEffect, useState } from 'react';
+import { connect } from 'react-redux';
+import { fetchdUsersWithinDistance, updateSingleUser } from '../store/users';
+import { loadMap, getGeoLocationFromBrowser } from '../../Util/loadMap';
 
 export const UserPage = (props) => {
   const [coords, setCoords] = useState([null, null]);
@@ -19,13 +19,8 @@ export const UserPage = (props) => {
   }, []);
 
   useEffect(() => {
-    async function setCurrentPosition() {
-      // if (!myMap && coords[0]) {
-      //   console.log(`from coords`, coords);
-      //   var myMap = loadMap("map", coords[0], coords[1]);
-      //   L.marker([coords[0], coords[1]]).addTo(myMap).bindPopup("<p>YOU</p>");
-
-      await props.updateUser(props.singleUser.id, {
+    function setCurrentPosition() {
+      props.updateUser(props.singleUser.id, {
         latitude: coords[0],
         longitude: coords[1],
       });
@@ -44,12 +39,12 @@ export const UserPage = (props) => {
 
   return (
     <div>
-      <div className="userSdashboard">
+      <div className='userSdashboard'>
         <select onChange={(e) => handleChange(e)}>
           <option></option>
-          <option value="1">1 Mile</option>
-          <option value="5">5 Miles</option>
-          <option value="10">10 Miles</option>
+          <option value='1'>1 Mile</option>
+          <option value='5'>5 Miles</option>
+          <option value='10'>10 Miles</option>
         </select>
       </div>
       {/* <div className="leafMap" id="map"></div> */}
@@ -60,7 +55,7 @@ export const UserPage = (props) => {
             return (
               <div
                 key={person.id}
-                className="nearby_users"
+                className='nearby_users'
                 onClick={() => props.history.push(`/profile/${person.id}`)}
               >
                 <h3>{person.username}</h3>
