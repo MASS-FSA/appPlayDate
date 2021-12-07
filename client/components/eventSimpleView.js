4f3635b9735cce0fe52ecb354112a57a72fe200f
Anatoly Tsinker <anatoly.tsiner12@gmail.com>
2021-12-06 22:54:34 -0500

    added link to image in eventSimpleView

--- client/components/eventSimpleView.js
+++ client/components/eventSimpleView.js
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from "react";
+import { Link } from 'react-router-dom'
 
 //  Gets its props passed in through map. Currently Used in places.js component
 const EventSimpleView = (props) => {
-  const {name, image, location, time} = props.event
-
+  const {id, name, image, location, time} = props.event
+  const goTo = `/events/${id}`
   return (
     <div>
       <h6>{name}</h6>
-      <img src={image}/>
+      <Link to={goTo}>
+        <img src={image}/>
+      </Link>
       <p>Where: {location}</p>
       <p>At: {time}</p>
     </div>
