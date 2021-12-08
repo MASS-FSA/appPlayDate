49b7e60af1917331e21d2b0d1020b6b33f5d7a83
Anatoly Tsinker <anatoly.tsiner12@gmail.com>
2021-12-08 01:01:16 -0500

    fixed problem with making friends in auth api route

--- server/auth/index.js
+++ server/auth/index.js
@@ -15,18 +15,28 @@ router.post('/login', async (req, res, next) => {
 router.post('/signup', async (req, res, next) => {
   try {
     const user = await User.create(req.body)
-    const user1 = await User.findByPk(1)
-    const user2 = await User.findByPk(2)
-    const user3 = await User.findByPk(3)
-    const user4 = await User.findByPk(4)
-    await user.addAddressee(user1)
-    await user.addAddressee(user2)
-    await user.addAddressee(user3)
-    await user.addAddressee(user4)
-    const friendShips = await Friend.findAll({where: {userId: user.id}})
-    await Promise.all(friendShips.map((friendship)=>{
-      return friendship.update({status:'accepted'})
-    }))
+    if(user) {
+      const user1 = await User.findByPk(1)
+      const user2 = await User.findByPk(2)
+      const user3 = await User.findByPk(3)
+      const user4 = await User.findByPk(4)
+      await user.addAddressee(user1)
+      await user1.addAddressee(user)
+      await user.addAddressee(user2)
+      await user2.addAddressee(user)
+      await user.addAddressee(user3)
+      await user3.addAddressee(user)
+      await user.addAddressee(user4)
+      await user4.addAddressee(user)
+      const friendShips = await Friend.findAll({where: {userId: user.id}})
+      await Promise.all(friendShips.map((friendship)=>{
+        return friendship.update({status:'accepted'})
+      }))
+      const reverse = await Friend.findAll({where: {AddresseeId: user.id}})
+      await Promise.all(reverse.map((friendship)=>{
+        return friendship.update({status:'accepted'})
+      }))
+    }
     res.send({token: await user.generateToken()})
   } catch (err) {
     if (err.name === 'SequelizeUniqueConstraintError') {
